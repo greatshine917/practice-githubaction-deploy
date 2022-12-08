38705dd87476c418051671d4c7f7a7978871caf7
greatshine917 <j01039519778@gmail.com>
2022-12-08 13:44:42 +0900

    first commit

---
+++ src/test/java/com/codestates/practicegithubActiondeploy2/PracticeGithubActionDeploy2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.practicegithubActiondeploy2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeGithubActionDeploy2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
